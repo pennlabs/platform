92a9d9cad470c6cf452ece139cb7e59a38148014
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-10-14 14:33:57 -0400

    Use custom user model for knox

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from .views import RegistrationView, LoginView, UserView
+from knox.views import LogoutView, LogoutAllView
 from django.urls import path, include
 
 urlpatterns = [
     path("register/", RegistrationView.as_view()),
     path("login/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view()),
+    path("logoutall/", LogoutAllView.as_view()),
     path("users/", UserView.as_view({'get': 'list'}))
 ]
