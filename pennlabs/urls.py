619ce916eab92690b0e97adbd8be3db56d3b9db4
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-11-03 14:46:26 -0400

    Add static file for deployment

--- pennlabs/urls.py
+++ pennlabs/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('api/auth/', include('knox.urls')),
     path('api/', include('api.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
