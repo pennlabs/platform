6616f7b57dc8a55761ce44272bf244090de9294b
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-11-04 13:14:01 -0500

    Add services views

--- services/views.py
+++ services/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from services.models import Service, Update
+from services.serializers import ServiceSerializer, UpdateSerializer
 
-# Create your views here.
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    http_method_names = ['get']
+
+
+class UpdateViewSet(viewsets.ModelViewSet):
+    queryset = Update.objects.all()
+    serializer_class = UpdateSerializer
+    http_method_names = ['get']
