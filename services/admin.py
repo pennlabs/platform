9b9f9efd9d91599e0beb10a7e117b6601127997c
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-11-04 13:13:47 -0500

    Add models to services admin

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from services.models import Service, Endpoint, Update
 
-# Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Endpoint)
+admin.site.register(Update)
