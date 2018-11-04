f0abf06c81ce025d068c1c941324c1b803cb5487
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-11-04 13:13:14 -0500

    Remove Update and Event seralizers from API

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from accounts.models import User
 from accounts.serializers import UserSerializer
-from api.models import Member, Team, Role, Update, Event
+from api.models import Member, Team, Role
 
 
 class TeamSerializer(serializers.ModelSerializer):
@@ -24,15 +24,3 @@ class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = ('user', 'bio', 'location', 'teams', 'roles', 'url', 'photo', 'linkedin', 'website', 'github', 'year_joined', 'alumnus')
-
-
-class UpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Update
-        fields = ('product', 'title', 'body')
-
-
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('name', 'date', 'location', 'start_time', 'end_time', 'link', 'free_food')
