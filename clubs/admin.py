a856b95ac6c44ad3c026afeaf3c4c366170bc742
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-11-11 12:09:25 -0500

    Fix clubs.Event ModelAdmin

--- clubs/admin.py
+++ clubs/admin.py
@@ -4,8 +4,6 @@ from clubs.models import Club, Event
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'club', 'time')
-    list_filter = ()
-    raw_id_fields = ()
 
 
 admin.site.register(Club)
