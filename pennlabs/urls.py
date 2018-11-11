7be1a408349f07c80fe8c9092f7607ab7a08dd92
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-11-11 11:30:47 -0500

    Add documentation framework

--- pennlabs/urls.py
+++ pennlabs/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('org/', include('org.urls')),
-    path('clubs/', include('clubs.urls'))
+    path('clubs/', include('clubs.urls')),
+    path('documentation/', include_docs_urls(title="Platform Documentation")),
 ]
