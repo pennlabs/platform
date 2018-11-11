c26b6b57f6ea10835f3879a0fdab63ead3931c1a
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-11-11 12:02:31 -0500

    Add clubs.Event ModelAdmin

--- clubs/admin.py
+++ clubs/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from clubs.models import Club, Event
 
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'club', 'time')
+    list_filter = ()
+    raw_id_fields = ()
+
+
 admin.site.register(Club)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
