0b21ca98a15b7ecbac000e98ebc74e65b51544a1
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-11-13 17:28:48 -0500

    Remove unnecessary whitenoise wsgi wrapper

--- pennlabs/wsgi.py
+++ pennlabs/wsgi.py
@@ -8,13 +8,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pennlabs.settings.production')
 
 application = get_wsgi_application()
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, "static"))
