97f32f6f0e0a6121f9979ecbcfeedd6fb5a9026c
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-12-03 22:38:28 -0500

    Add club detail view

--- engagement/urls.py
+++ engagement/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from engagement.views import ClubViewSet, EventViewSet
+from engagement.views import ClubViewSet, ClubDetail, EventViewSet
 
 
 urlpatterns = [
     path("clubs/", ClubViewSet.as_view({'get': 'list'})),
-    path("events/", EventViewSet.as_view({'get': 'list'})),
+    path('clubs/<str:pk>/', ClubDetail.as_view()),
+    path("events/", EventViewSet.as_view({'get': 'list'}))
 ]
--- engagement/views.py
+++ engagement/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework import viewsets, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from engagement.models import Club, Event
 from engagement.serializers import ClubSerializer, EventSerializer
 
@@ -11,6 +14,21 @@ class ClubViewSet(viewsets.ModelViewSet):
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
     http_method_names = ['get']
+
+class ClubDetail(APIView):
+    """
+    Return a single club.
+    """
+    def get_object(self, pk):
+        try:
+            return Club.objects.get(pk=pk)
+        except Club.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        club = self.get_object(pk)
+        serializer = ClubSerializer(club)
+        return Response(serializer.data)
 
 
 class EventViewSet(viewsets.ModelViewSet):
