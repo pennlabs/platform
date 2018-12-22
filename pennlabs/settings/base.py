dc4265457000fad5a70f27fe0890616ea777ef23
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-12-22 10:26:28 -0500

    Add DRF Api Key

--- pennlabs/settings/base.py
+++ pennlabs/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_api_key',
     'shortener.apps.ShortenerConfig',
     'org',
     'accounts',
