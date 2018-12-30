1561b86f87d8eee1067086982ae146a6abe5c1c1
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-12-29 21:39:07 -0500

    Remove refresh JWT endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from accounts.views import LoginView
-from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     path("login/", LoginView.as_view()),
-    path("refresh/", refresh_jwt_token),
     path("verify/", verify_jwt_token),
 ]
