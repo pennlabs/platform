9bf07a9034abd47c446e5da370320bf8754136b8
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-12-30 21:22:55 -0500

    Fix authentication mixins

--- accounts/auth.py
+++ accounts/auth.py
@@ -1,40 +1,44 @@
-# from knox.auth import TokenAuthentication
-from rest_framework import exceptions
-from org.models import Member
-from rest_framework import generics
+import jwt
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import exceptions, generics
 from rest_framework.authentication import get_authorization_header
 from rest_framework.permissions import IsAuthenticated
-from django.utils.translation import ugettext_lazy as _
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_jwt.settings import api_settings
+from org.models import Member
+
+jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
 
 
-# class LabsTokenAuthentication(TokenAuthentication):
-#     def authenticate(self, request):
-#         auth = get_authorization_header(request).split()
-#         prefix = "Token".encode()
-#         if not auth or auth[0].lower() != prefix.lower():
-#             return None
-#         if len(auth) == 1:
-#             msg = _('Invalid token header. No credentials provided.')
-#             raise exceptions.AuthenticationFailed(msg)
-#         elif len(auth) > 2:
-#             msg = _('Invalid token header. '
-#                     'Token string should not contain spaces.')
-#             raise exceptions.AuthenticationFailed(msg)
-#         (user, auth_token) = super().authenticate(request)
-#         if Member.objects.filter(user=user).exists():
-#             return (user, auth_token)
-#         else:
-#             raise exceptions.AuthenticationFailed(
-#                 _('Authentication Failed. User is not a Penn Labs member'))
+class LabsTokenAuthentication(JSONWebTokenAuthentication):
+    def authenticate(self, request):
+        jwt_value = self.get_jwt_value(request)
+        if jwt_value is None:
+            return None
+        try:
+            payload = jwt_decode_handler(jwt_value)
+        except jwt.ExpiredSignature:
+            msg = _('Signature has expired.')
+            raise exceptions.AuthenticationFailed(msg)
+        except jwt.DecodeError:
+            msg = _('Error decoding signature.')
+            raise exceptions.AuthenticationFailed(msg)
+        except jwt.InvalidTokenError:
+            raise exceptions.AuthenticationFailed()
+
+        (user, payload) = super().authenticate(request)
+        if hasattr(user.student, 'member'):
+            return (user, payload)
+        else:
+            raise exceptions.AuthenticationFailed(
+                _('Authentication Failed. User is not a Penn Labs member'))
 
 
 class PennAuthMixin(generics.GenericAPIView):
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
-    pass
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
 
 class LabsAuthMixin(generics.GenericAPIView):
-    # authentication_classes = (LabsTokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
-    pass
+    authentication_classes = (LabsTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
