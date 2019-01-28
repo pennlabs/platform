bd87e6f8b8f7c4f65cb0a1347123c101e553ceb4
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-01-27 20:26:04 -0500

    Use Application secret during login flow

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,14 @@
 from django.contrib import auth
 from django.http import HttpResponseServerError
+from django.http.response import HttpResponseBadRequest
 from django.shortcuts import redirect
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
 from rest_framework_api_key.crypto import hash_token
 from rest_framework_api_key.models import APIKey
 from rest_framework_api_key.settings import TOKEN_HEADER, SECRET_KEY_HEADER
 from rest_framework_jwt.settings import api_settings
 from accounts.models import Application
+from accounts.utils import hash_client_secret
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
@@ -16,29 +16,36 @@ jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 class LoginView(generics.GenericAPIView):
     """
-    Log in a user. API Key protected.
+    Log in a user.
     """
     def login_redirect(self, redirect_uri):
         return redirect('https://auth.pennlabs.org/login?redirect_uri=' + redirect_uri)
 
     def get(self, request):
-        # Validate API Key or redirect to Shibboleth
+        # Get body and header variables
         redirect_uri = request.GET.get('redirect_uri', '')
-        application = Application.objects.filter(redirect_uri=redirect_uri, revoked=False).first()
-        if application is None:
-            raise ValidationError({"redirect_uri": "Invalid redirect_uri"})
+        client_id = request.GET.get('client_id', '')
+        client_secret = request.GET.get('client_secret', '')
         token = request.META.get(TOKEN_HEADER, '')
         secret_key = request.META.get(SECRET_KEY_HEADER, '')
-        if not token or not secret_key:
-            return self.login_redirect(redirect_uri)
-        api_key = APIKey.objects.filter(token=token, revoked=False).first()
-        if api_key is None:
-            return self.login_redirect(redirect_uri)
-        hashed_token = hash_token(token, secret_key)
-        if hashed_token != api_key.hashed_token:
+
+        # Validate redirect_uri or invalidate request
+        application = Application.objects.filter(redirect_uri=redirect_uri, revoked=False).first()
+        if application is None:
+            return HttpResponseBadRequest("Invalid redirect_uri.")
+
+        # Validate Application client id and secret and redirect to auth (Product initiating request)
+        hased_client_secret = hash_client_secret(client_id, client_secret)
+        if hased_client_secret == application.hashed_secret:
             return self.login_redirect(redirect_uri)
 
-        # Use provided headers to login user
+        # Validate API token and secret or invalidate request (Auth initiating request)
+        api_key = APIKey.objects.filter(token=token, revoked=False).first()
+        hashed_token = hash_token(token, secret_key)
+        if api_key is None or hashed_token != api_key.hashed_token:
+            return HttpResponseBadRequest("Invalid request.")
+
+        # Request is from auth. Use provided headers to login user
         pennkey = request.META.get('HTTP_EPPN', '').lower().split('@')[0]
         first_name = request.META.get('HTTP_GIVENNAME', '').lower().capitalize()
         last_name = request.META.get('HTTP_SN', '').lower().capitalize()
