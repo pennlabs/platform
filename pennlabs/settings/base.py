d16a394c003af9fd6aaed71c6ddca1e33d2a1c8f
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-02-08 19:07:58 -0500

    Add OAuth2 apps

--- pennlabs/settings/base.py
+++ pennlabs/settings/base.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_api_key',
-    'rest_framework_simplejwt',
+    'oauth2_provider',
+    'corsheaders',
     'shortener.apps.ShortenerConfig',
     'org',
     'accounts',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
