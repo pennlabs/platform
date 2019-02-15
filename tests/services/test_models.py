2dc63efcc62ce9bfec11e118a3a112ab26e3ec2a
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-02-14 23:02:29 -0500

    Add services tests

---
+++ tests/services/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from org.models import Team
+from services.models import Endpoint, Service, Update
+
+
+class ServiceTestCase(TestCase):
+    def setUp(self):
+        self.team = Team.objects.create(name='Directors', tagline='Direct things', description='Important people',
+                                        order=1, url='https://pennlabs.org')
+        self.service = Service.objects.create(name="Service", team=self.team)
+
+    def test_str(self):
+        self.assertEqual(str(self.service), self.service.name)
+
+
+class EndpointTestCase(TestCase):
+    def setUp(self):
+        self.endpoint = Endpoint.objects.create(url='platform')
+
+    def test_str(self):
+        self.assertEqual(str(self.endpoint), self.endpoint.url)
+
+
+class UpdateTestCase(TestCase):
+    def setUp(self):
+        self.team = Team.objects.create(name='Directors', tagline='Direct things', description='Important people',
+                                        order=1, url='https://pennlabs.org')
+        self.service = Service.objects.create(name="Service", team=self.team)
+        self.update = Update.objects.create(service=self.service)
+
+    def test_str(self):
+        self.assertEqual(str(self.update), self.update.title)
