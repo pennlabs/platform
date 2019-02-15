74f588dce42aaa6a93baa3d91794f28b1caf06b1
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-02-14 20:45:09 -0500

    Update auth mixins

--- accounts/auth.py
+++ accounts/auth.py
@@ -1,15 +1,23 @@
+from django.http import HttpResponseForbidden
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import exceptions, generics
 from rest_framework.authentication import get_authorization_header
 from rest_framework.permissions import IsAuthenticated
+from oauth2_provider.views import ProtectedResourceView
+from oauth2_provider.views.mixins import ProtectedResourceMixin
 
 
-class PennAuthMixin(generics.GenericAPIView):
-    # authentication_classes = (JWTTokenUserAuthentication,)
-    permission_classes = (IsAuthenticated,)
+class LabsMixin(ProtectedResourceMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if hasattr(request.user, 'student') and hasattr(request.user.student, 'member'):
+            return super().dispatch(request, *args, **kwargs)
+        return HttpResponseForbidden()
 
 
-class LabsAuthMixin(generics.GenericAPIView):
-    # authentication_classes = (LabsTokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+class PennView(ProtectedResourceView):
+    pass
+
+
+class LabsView(LabsMixin, ProtectedResourceView):
+    pass
