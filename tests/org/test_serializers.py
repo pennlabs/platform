ddccd469f28a31be96842f79478c1e01aaeaf1db
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-02-15 21:04:16 -0500

    Fix org serializer tests

--- tests/org/test_serializers.py
+++ tests/org/test_serializers.py
@@ -10,18 +10,17 @@ from org.serializers import TeamSerializer
 
 class TeamSerializerTestCase(TestCase):
     def setUp(self):
+        self.date = datetime.datetime(2019, 1, 1, tzinfo=pytz.UTC)
         self.team = Team.objects.create(name='Platform', tagline='Break twice deploy once', description='Important',
                                         order=2, url='https://pennlabs.org')
         self.director_role = Role.objects.create(name='Z-Director', description='Important people', order=1)
         self.backend_role = Role.objects.create(name='Backend Engineer', description='Important stuff', order=2)
-        self.director = User.objects.create_user(username='z-director', password='secret',
-                                                 date_joined=datetime.datetime(2019, 1, 1, tzinfo=pytz.UTC))
-        self.backend = User.objects.create_user(username='backend', password='secret',
-                                                date_joined=datetime.datetime(2019, 1, 1, tzinfo=pytz.UTC))
-        self.director_member = Member.objects.create(student=self.director.student, year_joined=datetime.date.today(),
+        self.director = User.objects.create_user(username='z-director', password='secret', date_joined=self.date)
+        self.backend = User.objects.create_user(username='backend', password='secret', date_joined=self.date)
+        self.director_member = Member.objects.create(student=self.director.student, year_joined=self.date,
                                                      team=self.team, url='director')
         self.director_member.roles.add(self.director_role)
-        self.backend_member = Member.objects.create(student=self.backend.student, year_joined=datetime.date.today(),
+        self.backend_member = Member.objects.create(student=self.backend.student, year_joined=self.date,
                                                     team=self.team, url='backend')
         self.backend_member.roles.add(self.backend_role)
         self.serializer = TeamSerializer(self.team)
