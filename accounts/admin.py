acbeb9b20542071567de74a165fa58ee095ee421
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-05 17:04:08 -0400

    Update accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from accounts.models import Student
+from accounts.models import PennAffiliation, ProductPermissions, Student, User
 
 
-class StudentAdmin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     readonly_fields = ('uuid',)
 
 
-admin.site.register(Student, StudentAdmin)
+admin.site.register(PennAffiliation)
+admin.site.register(ProductPermissions)
+admin.site.register(Student)
+admin.site.register(User, UserAdmin)
