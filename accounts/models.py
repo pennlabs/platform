560ac217c2fd84e6cd189fee8842eb82d5fb02f5
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-05 17:03:20 -0400

    Redo accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,31 +1,35 @@
 import uuid
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
+
+class User(AbstractUser):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    affiliation = models.ManyToManyField('PennAffiliation')
+    product_permissions = models.ManyToManyField('ProductPermissions')
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, blank=True)
+    user = models.OneToOneField(get_user_model(), on_delete=models.DO_NOTHING)
     major = models.CharField(max_length=255, blank=True)
     school = models.CharField(max_length=255, blank=True)
-
-    def get_uuid(self):
-        return str(self.uuid)
 
     def __str__(self):
         return self.user.username
 
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Student.objects.create(user=instance)
+class PennAffiliation(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.student.save()
+class ProductPermissions(models.Model):
+    id = models.SlugField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
