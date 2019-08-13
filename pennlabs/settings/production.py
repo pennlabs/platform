a6508faa0efca28d1e10e733815ee178ff435116
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-08-13 17:11:34 -0400

    Remove whitenoise configuration

--- pennlabs/settings/production.py
+++ pennlabs/settings/production.py
@@ -6,15 +6,6 @@ from pennlabs.settings.base import *
 
 DEBUG = False
 
-# Disable Django's own staticfiles handling in favour of WhiteNoise, for
-# greater consistency between gunicorn and `./manage.py runserver`. See:
-# http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
-MIDDLEWARE.remove('django.middleware.security.SecurityMiddleware')
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-] + MIDDLEWARE
-
 # Fix MySQL Emoji support
 DATABASES['default']['OPTIONS'] = {'charset': 'utf8mb4'}
 
