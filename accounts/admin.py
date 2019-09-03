840067a76b1bd0bdc28d756a84a9bf677ec9a7a1
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-09-02 22:16:12 -0400

    Add features to User Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,9 @@ from accounts.models import PennAffiliation, ProductPermission, Student, User
 
 class UserAdmin(admin.ModelAdmin):
     readonly_fields = ('uuid',)
+    search_fields = ('username', 'first_name', 'last_name')
+    list_display = ('username', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
 
 
 admin.site.register(PennAffiliation)
