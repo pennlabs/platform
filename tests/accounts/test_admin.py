a9802d3118bc2b74f6b90f15e10065de4e71bdd5
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-09-03 17:46:46 -0400

    Add admin tests

---
+++ tests/accounts/test_admin.py
@@ -0,0 +1,21 @@
+from django.contrib.admin.sites import AdminSite
+from django.test import TestCase
+
+from accounts.admin import StudentAdmin
+from accounts.models import Student, User
+
+
+class StudentAdminTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user', first_name='First', last_name='Last')
+        self.student = Student.objects.create(user=self.user)
+        self.sa = StudentAdmin(Student, AdminSite())
+
+    def test_username(self):
+        self.assertEqual(self.sa.username(self.student), self.user.username)
+
+    def test_first_name(self):
+        self.assertEqual(self.sa.first_name(self.student), self.user.first_name)
+
+    def test_last_name(self):
+        self.assertEqual(self.sa.last_name(self.student), self.user.last_name)
