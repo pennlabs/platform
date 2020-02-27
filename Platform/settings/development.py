fb64fda94c4721c237927e8f63cc9c98149c7d77
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-02-27 17:49:16 -0500

    Shib admin login

--- Platform/settings/development.py
+++ Platform/settings/development.py
@@ -7,3 +7,6 @@ INSTALLED_APPS += ["django_extensions", "debug_toolbar"]
 
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 INTERNAL_IPS = ["127.0.0.1"]
+
+# Disable admin login through shibboleth
+SHIB_ADMIN = False
--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import Permission
+from django.shortcuts import redirect
+from django.urls import reverse
 
 from accounts.models import Student, User
 
@@ -41,3 +44,25 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.register(Permission)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(User, UserAdmin)
+
+
+class LabsAdminSite(admin.AdminSite):
+    """
+    Custom admin site that redirects users to log in through shibboleth
+    instead of logging in with a username and password
+    """
+
+    def login(self, request, extra_context=None):
+        if not request.user.is_authenticated:
+            return redirect(reverse("accounts:login") + "?next=" + request.GET.get("next"))
+        return super().login(request, extra_context)
+
+
+if settings.SHIB_ADMIN:
+    """
+    Replace the default admin site with a custom one to log in users through platform.
+    Also copy all registered models from the default admin site
+    """
+    labs_admin = LabsAdminSite()
+    labs_admin._registry = admin.site._registry
+    admin.site = labs_admin
