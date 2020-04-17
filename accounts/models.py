063464c4351482439ba4748f07dcfebfa885a6c7
Lanting Chiang <lantingchiang@gmail.com>
2020-04-17 04:25:11 -0400

    added email model

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     # from AbstractUser that contains the user's PennKey
     pennid = models.IntegerField(primary_key=True)
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-
+    preferred_name = models.CharField(max_length=100, blank=True)
 
 class Student(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.DO_NOTHING)
@@ -19,3 +19,14 @@ class Student(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Email(models.Model):
+    # optional additional emails that users can add
+    email = models.EmailField()
+    user = models.OneToOneField(get_user_model(), on_delete=models.DO_NOTHING, through='EmailVerification')
+
+class EmailVerification(models.Model):
+    # through model to keep track of whether email was verified
+    user = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING)
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    verified = models.BooleanField(default=False)
