a217e50f02907ac6116ba06aca2688bece834662
Lanting Chiang <lantingchiang@gmail.com>
2020-05-16 08:52:46 -0400

    fix formatting in migrations file

--- accounts/migrations/0003_auto_20200421_2056.py
+++ accounts/migrations/0003_auto_20200421_2056.py
@@ -9,38 +9,63 @@ import phonenumber_field.modelfields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0002_auto_20200213_1711'),
+        ("accounts", "0002_auto_20200213_1711"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='student',
-            name='graduation_year',
+            model_name="student",
+            name="graduation_year",
             field=models.CharField(blank=True, max_length=4),
         ),
         migrations.AddField(
-            model_name='user',
-            name='preferred_name',
+            model_name="user",
+            name="preferred_name",
             field=models.CharField(blank=True, max_length=100),
         ),
         migrations.CreateModel(
-            name='PhoneNumber',
+            name="PhoneNumber",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('phone_number', phonenumber_field.modelfields.PhoneNumberField(max_length=128, region=None)),
-                ('primary_number', models.BooleanField(default=False)),
-                ('verified', models.BooleanField(default=False)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.DO_NOTHING, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                (
+                    "phone_number",
+                    phonenumber_field.modelfields.PhoneNumberField(max_length=128, region=None),
+                ),
+                ("primary_number", models.BooleanField(default=False)),
+                ("verified", models.BooleanField(default=False)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.DO_NOTHING, to=settings.AUTH_USER_MODEL
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Email',
+            name="Email",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('primary_email', models.BooleanField(default=False)),
-                ('email', models.EmailField(max_length=254)),
-                ('verified', models.BooleanField(default=False)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.DO_NOTHING, related_name='emails', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("primary_email", models.BooleanField(default=False)),
+                ("email", models.EmailField(max_length=254)),
+                ("verified", models.BooleanField(default=False)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.DO_NOTHING,
+                        related_name="emails",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
