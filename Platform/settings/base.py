7236064cb36e4c299702134b0fd9b36294a1b1e4
Lanting Chiang <lantingchiang@gmail.com>
2020-05-16 08:36:52 -0400

    add emails & phone# models

--- Platform/settings/base.py
+++ Platform/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "services.apps.ServicesConfig",
     "application",
+    "phonenumber_field",
 ]
 
 
--- accounts/models.py
+++ accounts/models.py
@@ -13,31 +13,27 @@ class User(AbstractUser):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     preferred_name = models.CharField(max_length=100, blank=True)
 
+
 class Student(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.DO_NOTHING)
     major = models.CharField(max_length=255, blank=True)
     school = models.CharField(max_length=255, blank=True)
+    graduation_year = models.CharField(max_length=4, blank=True)
 
     def __str__(self):
         return self.user.username
 
-class Email(models.Model):
-    # optional additional user emails 
-    email = models.EmailField()
-    user = models.OneToOneField(get_user_model(), on_delete=models.DO_NOTHING, through='EmailVerification')
 
-class EmailVerification(models.Model):
-    # through model to keep track of whether email was verified
-    user = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING)
-    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+class Email(models.Model):
+    user = models.ForeignKey(get_user_model(), related_name="emails", on_delete=models.DO_NOTHING)
+    email = models.EmailField()
+    # whether this email is the primary email
+    primary_email = models.BooleanField(default=False)
     verified = models.BooleanField(default=False)
 
-class PhoneNumber(models.Model):
-    # optional user phone numbers
-    phone_number = PhoneNumberField()
-    user = models.OneToOneField(get_user_model(), on_delete=models.DO_NOTHING, through='PhoneNumberVerification')
 
-class PhoneNumberVerification(models.Model):
+class PhoneNumber(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING)
-    email = models.ForeignKey(PhoneNumber, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField()
+    primary_number = models.BooleanField(default=False)
     verified = models.BooleanField(default=False)
