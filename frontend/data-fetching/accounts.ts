b4d3c0fd41d42d1fe411078857b204b220fa9fac
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-02-23 11:35:55 -0500

    Add additional data functions

--- frontend/data-fetching/accounts.ts
+++ frontend/data-fetching/accounts.ts
@@ -36,3 +36,42 @@ export const verifyContact = async (
         throw new Error(body.detail);
     }
 };
+
+export const createContact = async (type: ContactType, value: string) => {
+    // TODO: use `value` field on both models?
+    const key = type === ContactType.Email ? "email" : "phone_number";
+    const payload = { [key]: value };
+    const res = await doApiRequest(`/accounts/me/${type}/`, {
+        method: "POST",
+        body: payload,
+    });
+
+    if (!res.ok) {
+        const body = await res.json();
+        throw new Error(body.detail);
+    }
+};
+
+export const makePrimaryContact = async (type: ContactType, id: number) => {
+    const payload = { primary: true };
+    const res = await doApiRequest(`/accounts/me/${type}/${id}/`, {
+        method: "PATCH",
+        body: payload,
+    });
+
+    if (!res.ok) {
+        const body = await res.json();
+        throw new Error(body.detail);
+    }
+};
+
+export const deleteContact = async (type: ContactType, id: number) => {
+    const res = await doApiRequest(`/accounts/me/${type}/${id}/`, {
+        method: "DELETE",
+    });
+
+    if (!res.ok) {
+        const body = await res.json();
+        throw new Error(body.detail);
+    }
+};
