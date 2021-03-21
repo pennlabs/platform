9fb8944105a4fbeb1452a292dabb93c4783337d7
vsingh18567 <59497793+vsingh18567@users.noreply.github.com>
2021-03-21 12:52:47 -0400

    linting

--- accounts/management/commands/populate_users.py
+++ accounts/management/commands/populate_users.py
@@ -1,7 +1,6 @@
 import random
 import uuid
 
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.core.management import BaseCommand
