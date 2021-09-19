fde1fce8dcc4ab192249e3499e337aacb1d80520
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-09-18 20:00:58 -0400

    Add django 3.2 config

--- Platform/settings/base.py
+++ Platform/settings/base.py
@@ -96,6 +96,9 @@ DATABASES = {
     "default": dj_database_url.config(default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3"))
 }
 
+# Set default PK type
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
