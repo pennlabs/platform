2f28290518fdeebf8de73b8d4caa578e3e4a35ae
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-10-03 11:26:43 -0400

    CI + K8s changes

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -1,26 +1,10 @@
-import { Construct } from "constructs";
-import { App, Stack, Workflow } from "cdkactions";
-import { DeployJob, DjangoProject } from "@pennlabs/kraken";
-
-export class PlatformStack extends Stack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
-    const workflow = new Workflow(this, 'build-and-deploy', {
-      name: 'Build and Deploy',
-      on: 'push',
-    });
-
-    const platformJob = new DjangoProject(workflow, {
-      projectName: 'Platform',
-      imageName: 'platform',
-    });
-
-    new DeployJob(workflow, {}, {
-      needs: [platformJob.publishJobId]
-    });
-  }
-}
+import { App } from "cdkactions";
+import { LabsApplicationStack } from "@pennlabs/kraken";
 
 const app = new App();
-new PlatformStack(app, 'platform');
+new LabsApplicationStack(app, {
+  djangoProjectName: 'Platform',
+  dockerImageBaseName: 'platform',
+});
+
 app.synth();
