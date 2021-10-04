e6e032a85f03aa2aeb82158a5d6ef48fe4f58555
Ying Xuan <engyingxuan@gmail.com>
2021-10-04 16:15:53 -0400

    Ran lint locally this time

--- frontend/utils/sentry.tsx
+++ frontend/utils/sentry.tsx
@@ -1,16 +1,16 @@
-let Sentry;
+// let Sentry;
 
-const SENTRY_URL =
-    "https://a022d59af8cd4b6e928837a44239d8ac@sentry.pennlabs.org/12";
+// const SENTRY_URL =
+//     "https://a022d59af8cd4b6e928837a44239d8ac@sentry.pennlabs.org/12";
 const dev = process.env.NODE_ENV !== "production";
 
-if (process.browser) {
-    // If the code is running in user's browser
-    Sentry = require("@sentry/browser");
-} else {
-    // If code is running on the server
-    Sentry = require("@sentry/node");
-}
+// if (process.browser) {
+//     // If the code is running in user's browser
+//     Sentry = require("@sentry/browser");
+// } else {
+//     // If code is running on the server
+//     Sentry = require("@sentry/node");
+// }
 
 if (!dev) {
     // Sentry.init({ dsn: SENTRY_URL });
