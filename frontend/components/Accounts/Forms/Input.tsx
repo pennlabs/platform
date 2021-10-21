6c1a5cc38fc5875e7c472b750e952f3da04277c4
printer83mph <printer.83mph@gmail.com>
2021-10-21 19:23:45 -0400

    Remove todo hehe

--- frontend/components/Accounts/Forms/Input.tsx
+++ frontend/components/Accounts/Forms/Input.tsx
@@ -5,7 +5,6 @@ import { FormInput } from "../ui";
 export const FormikInput = ({ fieldName, ...props }) => {
     const [field, meta] = useField(fieldName);
 
-    // TODO: form validation (year should be constrained to current + 10)
     return (
         <FormInput
             /* eslint-disable */
