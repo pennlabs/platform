faea76253ce57aa6e7eb57d838c1fc11783d43e6
Rohan Moniz <rohan.moniz@gmail.com>
2023-10-13 17:32:25 -0400

    oops

--- backend/announcements/management/commands/populate_audiences.py
+++ backend/announcements/management/commands/populate_audiences.py
@@ -1,14 +1,6 @@
-import json
-import random
-
-from django.contrib.auth.models import Group
-from django.core.management import BaseCommand, call_command
+from django.core.management import BaseCommand
 
 from announcements.models import Audience
-
-
-with open("accounts/data/users.json") as f:
-    users = json.load(f)["users"]
 
 
 class Command(BaseCommand):
