4333f1d272e2996d7c62698c325019f11912d7bd
Rohan Moniz <rohan.moniz@gmail.com>
2023-11-02 12:55:07 -0400

    update Announcement serializer

--- backend/announcements/serializers.py
+++ backend/announcements/serializers.py
@@ -5,10 +5,41 @@ from announcements.models import Announcement, Audience
 class AudienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Audience
-        fields = "__all__"
+        fields = ("name",)
 
 
 class AnnouncementSerializer(serializers.ModelSerializer):
+    audiences = serializers.SlugRelatedField(many=True, slug_field="name", queryset=Audience.objects.all())
+
     class Meta:
         model = Announcement
-        fields = "__all__"
+        fields = ("id", "title", "message", "announcement_type", "release_time", "end_time", "audiences")
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["audiences"] = [audience.name for audience in instance.audiences.all()]
+        return representation
+
+    def to_internal_value(self, data):
+        audiences = data.get("audiences")
+        if isinstance(audiences, list):
+            audience_objs = []
+            for audience_name in audiences:
+                audience = Audience.objects.filter(name=audience_name).first()
+                if audience:
+                    audience_objs.append(audience)
+            data["audiences"] = audience_objs
+        return super().to_internal_value(data)
+
+    def create(self, validated_data):
+        audiences = validated_data.pop("audiences")
+        instance = Announcement.objects.create(**validated_data)
+        instance.audiences.set(audiences)
+        return instance
+
+    def update(self, instance, validated_data):
+        audiences = validated_data.pop("audiences", None)
+        super().update(instance, validated_data)
+        if audiences:
+            instance.audiences.set(audiences)
+        return instance
