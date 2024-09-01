b4b57815906e1b91f228d6d8352d52e326dcc47e
Eunsoo Shin <62971511+esinx@users.noreply.github.com>
2024-09-02 00:19:22 +0900

    fix(jwt): email (#177)

--- backend/accounts/oauth2_validator.py
+++ backend/accounts/oauth2_validator.py
@@ -16,7 +16,7 @@ class LabsOAuth2Validator(OAuth2Validator):
     def get_additional_claims(self, request):
         return {
             "name": request.user.preferred_name or request.user.get_full_name(),
-            "email": request.user.email,
+            "email": request.user.get_email(),
             "pennid": request.user.pennid,
             "is_staff": request.user.is_staff,
             "is_active": request.user.is_active,
