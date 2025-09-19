1dd70a733c13cdf1270965be4adb4e543e2afab5
Clayton Hickey <clayton@claytondoesthings.xyz>
2025-09-19 16:44:39 -0400

    Migrate to UV & fix some tests (#185)
    * Migrate to UV & fix some tests
    
    * :wq
    
    * Fix environment-dependent build failure by pinning Python version
    
    * Migrated workflow to uv
    
    * Added uv install to workflow
    
    * bump workflow python
    
    * fix uv version
    
    * Almost got deploy working
    
    * Fixed Dockerfile.dev
    
    * Finalize uv migration
    
    * Bump actions
    
    * Bump actions uv version
    
    * Confirm right dependency versions for actions
    
    * save
    
    * Might be ready for prod??
    
    * maybe this works?
    
    * Bump django base
    
    * Accept minor bumps for python 3.11
    
    ---------
    
    Co-authored-by: Christopher Liu <liuchris@seas.upenn.edu>

--- backend/tests/accounts/test_update_majors.py
+++ backend/tests/accounts/test_update_majors.py
@@ -16,22 +16,22 @@ class UpdateMajorsTestCase(TestCase):
         mock_source_file.return_value.text = self.html
         update_all_majors()
 
-        self.assertEquals(Major.objects.all().count(), 469)
+        self.assertEqual(Major.objects.all().count(), 469)
 
     def testBachelorMajorCount(self, mock_source_file):
         mock_source_file.return_value.text = self.html
         update_all_majors()
 
-        self.assertEquals(Major.objects.filter(degree_type="BACHELORS").count(), 215)
+        self.assertEqual(Major.objects.filter(degree_type="BACHELORS").count(), 215)
 
     def testMasterCount(self, mock_source_file):
         mock_source_file.return_value.text = self.html
         update_all_majors()
 
-        self.assertEquals(Major.objects.filter(degree_type="MASTERS").count(), 123)
+        self.assertEqual(Major.objects.filter(degree_type="MASTERS").count(), 123)
 
     def testProfessionalCount(self, mock_source_file):
         mock_source_file.return_value.text = self.html
         update_all_majors()
 
-        self.assertEquals(Major.objects.filter(degree_type="PROFESSIONAL").count(), 47)
+        self.assertEqual(Major.objects.filter(degree_type="PROFESSIONAL").count(), 47)
